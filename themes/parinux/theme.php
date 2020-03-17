0715a0e90747043ccec0db830315e150ef2749cd
Miraty <miraty@antopie.org>
2020-03-17 17:53:41 +0100

    Enhances contrast for Parinux's theme

--- themes/parinux/theme.php
+++ themes/parinux/theme.php
@@ -3,13 +3,13 @@
 $themeDimensionsIcons = array(16, 32, 48, 64, 96, 128, 192, 256, 384, 512); // Liste les dimensions des icones
 
 $variablesTheme = array(
-  "bg" => "#157097",
-  "bgField" => "#2794c2",
-  "bgTextField" => "#2794c2",
+  "bg" => "#14678b",
+  "bgField" => "#2186b1",
+  "bgTextField" => "#2186b1",
   "bgHelp" => "#118abe",
-  "border" => "#48aed9",
+  "border" => "#42a0c8",
   "borderHover" => "#87d1f1",
   "borderFocus" => "#e2f6ff",
-  "text" => "#d5f3ff",
-  "secondaryText" => "#65b7da"
+  "text" => "#ffffff",
+  "secondaryText" => "#bbe1f1"
 ); // Définit les couleurs du thème
