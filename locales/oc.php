049dcca74a1ad96801991317e2a2c3cdaa284417
quentin <quentinantonin@free.fr>
2020-12-31 19:56:18 +0100

    First translation into Occitan
    Translated in lengadocian Occitan, also called Central, General, Large.

---
+++ locales/oc.php
@@ -0,0 +1,52 @@
+<?php
+$loc = array(
+  'subtitle' => "Generator de còdis QR",
+  'description' => "Generatz de còdis QR liurament. Causissètz lo contengut, la talha, las colors…",
+
+  'label_content' => "Tèxt de codar",
+  'label_redondancy' => "Taus de redondància",
+  'label_margin' => "Talha del marge",
+  'label_size' => "Talha de l’imatge",
+  'label_bgColor' => "Color de fons",
+  'label_mainColor' => "Color del primièr plan",
+
+  'placeholder' => "Dintratz lo tèxt de codar en còdi QR",
+
+  'value_default' => "per defaut",
+
+  'help_content' => "
+    Podètz pas que codar lo tèxt que volètz.<br>
+    Los logicials que deschifran los còdis QR poirián suggerir de los dubrir dins de logicials especials, segon lor <a href='https://en.wikipedia.org/wiki/List_of_URI_schemes'>esquèma URI</a>.<br><br>
+    Per exemple, per dubrir la pagina web :<br>
+    https://www.domain.tld/<br><br>
+    Per enviar un corrièl :<br>
+    mailto:contact@domain.tld<br><br>
+    Per partejar de coordonadas geograficas :<br>
+    geo:48.867564,2.364057<br><br>
+    Per sonar un numèro de telefòn :<br>
+    tel:+33639981871
+  ",
+  'help_redondancy' => "La redondància es la duplicacion de las informacions al còdi QR per dire de corregir las errors pendent lo deschiframent. Un taux mai naut produirà un còdi mai grand, mas serà de melhor deschifrar corrèctament.",
+  'help_margin' => "Nombre de pixèls de la banda blanca a l’entorn del còdi QR.",
+  'help_size' => "Per quant deu la dimensions de l’imatge èsser multiplicada ?",
+
+  'button_create' => "Generar",
+  'button_download' => "Telecargar aqueste còdi QR",
+
+  'title_showOnlyQR' => "Mostrar sonque aqueste còdi QR",
+
+  'alt_help' => "Ajuda",
+  'alt_QR_before' => 'Significacion de còdi QR"',
+  'alt_QR_after' => '"',
+
+  'metaText_qr' => "
+    <h3>Qu’es aquò un còdi QR ?</h3>
+    Un còdi QR es un còdi de barras en doas dimensions dins lo qual lo tèxt es escrich en binari. Un aparalh equipat d’un capteur optic pòt lo deschifrar, amb l’aplicacion que cal.
+    <a lang='ca' href='https://ca.wikipedia.org/wiki/Codi_QR'>Còdi QR a la Wikipèdia</a>.
+  ",
+  'metaText_legal' => "LibreQR " . $libreqrVersion . " es un logicial liure que son <a href='https://code.antopie.org/miraty/libreqr/'>còdi font</a> es disponible jols tèrmes de la licéncia <abbr title='GNU Affero General Public License version 3 or any later version'><a href='LICENSE.html'>AGPLv3</a>+</abbr>.",
+
+  'opensearch_description' => "Generatz de còdis QR a partir de la barra de recèrca o d’adreça",
+  'opensearch_actionName' => "Generatz de còdis QR a partir de la barra de recèrca o d’adreça",
+);
+
