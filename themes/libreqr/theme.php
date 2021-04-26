b009740d6adf723e1744afbed928712a99ea705b
Miraty <miraty@antopie.org>
2021-04-27 00:24:20 +0200

    Add textarea-specific text color settings

--- themes/libreqr/theme.php
+++ themes/libreqr/theme.php
@@ -5,23 +5,25 @@ $themeDimensionsIcons = array(16, 32, 48, 64, 96, 128, 192, 256, 384, 512);
 
 $colorScheme = array(
   // Light theme
+  "text-light" => "#2a2a2a",
   "bg-light" => "white",
   "bgField-light" => "#eeeeee",
-  "bgTextField-light" => "#e5e5e5",
   "bgHelp-light" => "#ececec",
+  "bgTextarea-light" => "#e5e5e5",
+  "textareaText-light" => "#2a2a2a",
+  "textareaPlaceholder-light" => "#868686",
   "border-light" => "#65666b",
   "borderHover-light" => "#46484e",
   "borderFocus-light" => "#2a2a2a",
-  "text-light" => "#2a2a2a",
-  "secondaryText-light" => "#868686",
   // Dark theme
+  "text-dark" => "white",
   "bg-dark" => "#2a2a2a",
   "bgField-dark" => "#31363B",
-  "bgTextField-dark" => "#232629",
   "bgHelp-dark" => "#151616",
+  "bgTextarea-dark" => "#232629",
+  "textareaText-dark" => "white",
+  "textareaPlaceholder-dark" => "#bababa",
   "border-dark" => "#5f5f5f",
   "borderHover-dark" => "#808080",
-  "borderFocus-dark" => "white",
-  "text-dark" => "white",
-  "secondaryText-dark" => "#bababa"
+  "borderFocus-dark" => "white"
 );
